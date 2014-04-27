f466f5ba39bbb24d6ead200cb016a3ab4d6bfba9
Alex Komrakov <alexkomrakov@gmail.com>
2014-04-27 19:52:57 +0400

    Удалены неиспользуемые функции в js

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,18 @@
 var streamsApp = angular.module('dotaStreams', ['ngResource', 'ngSanitize', 'ngAnimate']);
 
-streamsApp.factory('Streams', ['$resource', '$sce', function($resource, $sce) {
-//	var firstChannel = rest().get();
-	function rest () {
-		return $resource('https://api.twitch.tv/kraken/streams', {}, {
+streamsApp.factory('Streams', ['$resource', '$sce', function ($resource, $sce) {
+    function rest() {
+        return $resource('https://api.twitch.tv/kraken/streams', {}, {
             get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', game: 'Dota 2'}}
         });
-	}
-	function show (channelName) {
-		if (!channelName)
-			channelName = 'starladder1';
-		return $sce.trustAsHtml('<object type="application/x-shockwave-flash" height="670" width="1140" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel='+channelName+'" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel='+channelName+'&auto_play=true&start_volume=25" /></object>');
-	}
-	return {
-		show: show,
-		rest: rest
-	};
+    }
+
+    return {
+        rest: rest
+    };
 }]);
 
-streamsApp.controller('streamList',['Streams', '$scope', '$interval', function(Streams, $scope, $interval) {
+streamsApp.controller('streamList', ['Streams', '$scope', '$interval', function (Streams, $scope, $interval) {
     $scope.config = {
         stream: '',
         startvolume: '50'
@@ -46,62 +40,3 @@ streamsApp.directive('stream', function () {
         template: '<object type="application/x-shockwave-flash" height="670" width="1140" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel={{config.stream.channel.name}}" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel={{config.stream.channel.name}}&auto_play=true&start_volume={{startvolume}}" /></object>'
     }
 });
-
-
-// streamsApp.directive('match', function() {
-//   return {
-//     require: 'ngModel',
-//     link: function(scope, elm, attrs, ctrl) {
-//       scope.$watch(attrs.match, function(newValue, oldValue) {
-//       	scope.match_newValue = newValue;
-//     	console.log(scope.match_newValue);		
-//     	console.log(scope.match_value);
-//     	console.log("----");
-    	
-//     	if (scope.match_newValue == scope.match_value) {
-//     		ctrl.$setValidity('pass_match', true);
-//     	} else {
-//     		ctrl.$setValidity('pass_match', false);
-//     	}
-//       });
-//       ctrl.$parsers.unshift(function(viewValue) {     	
-//        	scope.match_value = viewValue;
-//     	console.log(scope.match_newValue);		
-//     	console.log(scope.match_value);
-//     	console.log("----");
-
-//     	if (scope.match_newValue == scope.match_value) {
-//     		ctrl.$setValidity('pass_match', true);
-//     	} else {
-//     		ctrl.$setValidity('pass_match', false);
-//     	}
-//       });
-//     }
-//   };
-// });
-
-
-
-
-
-
-
-// streamsApp.directive('match', function() {
-//   return {
-//     require: 'ngModel',
-//     link: function(scope, elm, attrs, ctrl) {
-//       ctrl.$parsers.unshift(function(viewValue) {
-//         if (INTEGER_REGEXP.test(viewValue)) {
-//           // it is valid
-//           ctrl.$setValidity('integer', true);
-//           return viewValue;
-//         } else {
-//           // it is invalid, return undefined (no model update)
-//           ctrl.$setValidity('integer', false);
-//           return undefined;
-//         }
-//       });
-//     }
-//   };
-// });
-
