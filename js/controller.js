3781e14a6204a9e5498847e846c368946a11d26b
AlexKomrakov <a.komrakov@smart-crowd.ru>
2015-02-05 18:31:41 +0300

    + CS: GO link

--- js/controller.js
+++ js/controller.js
@@ -1,43 +1,66 @@
 var streamsApp = angular.module('dotaStreams', ['ngResource', 'ngSanitize', 'ngAnimate']);
 
-streamsApp.config(function($httpProvider) {
+streamsApp
+    .config(Config)
+    .filter('empty', Empty)
+    .service('Twitch', Twitch)
+    .service('SteamApi', SteamApi)
+    .service('GameSelector', GameSelector)
+    .controller('streamList', streamList)
+    .directive('stream', Stream);
+
+function Config($httpProvider) {
     //Enable cross domain calls
     $httpProvider.defaults.useXDomain = true;
 
     //Remove the header used to identify ajax call  that would prevent CORS from working
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
+}
 
-streamsApp.filter('empty', function() {
+function Empty() {
     return function(mixed_var) {
-            return (typeof mixed_var === 'undefined')
-                || mixed_var === ""
-                || mixed_var === 0
-                || mixed_var === "0"
-                || mixed_var === null
-                || mixed_var === false
-                || mixed_var === [];
-        }
+        return (typeof mixed_var === 'undefined')
+            || mixed_var === ""
+            || mixed_var === 0
+            || mixed_var === "0"
+            || mixed_var === null
+            || mixed_var === false
+            || mixed_var === [];
     }
-);
+}
 
-streamsApp.service('Twitch', function ($resource) {
-    var TwitchAPI = $resource('https://api.twitch.tv/kraken/streams', {}, {
-        get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', game: 'Dota 2'}}
-    });
+function GameSelector($rootScope) {
+    var GameSelector = this;
+    var gameList     = [ "Counter-Strike: Global Offensive", "Dota 2" ];
+    var game         = { id: "Dota 2" };
+    GameSelector.getGameList = function() { return gameList; };
+    GameSelector.getGame     = function() { return game; };
+    GameSelector.selectGame  = function(game_id) {
+        game.id = game_id;
+        $rootScope.$broadcast("changeGame");
+    }
+}
+
+function Twitch($rootScope, $resource, GameSelector) {
+    var Twitch = this;
     var data = { streams: [] };
+    var game = GameSelector.getGame();
+    var TwitchAPI = $resource('https://api.twitch.tv/kraken/streams', {}, {
+        get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK'}}
+    });
+    $rootScope.$on('changeGame', function(){ Twitch.updateStreams(); });
     this.updateStreams = function() {
-        TwitchAPI.get().$promise.then(function (result) {
+        TwitchAPI.get({game: game.id}).$promise.then(function (result) {
             if ('streams' in result && (typeof result.streams[0] != 'undefined')) data.streams = result.streams;
         });
     };
     this.getStreams = function() {
-        this.updateStreams();
+        Twitch.updateStreams();
         return data;
     };
-});
+}
 
-streamsApp.service('SteamApi', function ($resource){
+function SteamApi($resource) {
     var SteamApi = $resource('/live.php');
     var data = { games: [] };
     this.updateGames = function() {
@@ -49,9 +72,19 @@ streamsApp.service('SteamApi', function ($resource){
         this.updateGames();
         return data;
     };
-});
+}
 
-streamsApp.controller('streamList', function (Twitch, SteamApi, $scope, $interval) {
+function Stream() {
+    return {
+        restrict: 'E',
+        scope: { config: '=config' },
+        template: '<object type="application/x-shockwave-flash" height="670" width="1140" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel={{config.stream.channel.name}}" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel={{config.stream.channel.name}}&auto_play=true&start_volume={{startvolume}}" /></object>'
+    }
+}
+
+function streamList(Twitch, SteamApi, $scope, $interval, GameSelector) {
+    $scope.games = GameSelector.getGameList();
+    $scope.changeGame = GameSelector.selectGame;
     $scope.config = {
         stream: '',
         startvolume: '50'
@@ -59,15 +92,4 @@ streamsApp.controller('streamList', function (Twitch, SteamApi, $scope, $interva
     $scope.streamsContainer = Twitch.getStreams();
     $scope.gamesContainer = SteamApi.getGames();
     $interval(function () { Twitch.updateStreams(); SteamApi.updateGames(); }, 5 * 1000);
-});
-
-streamsApp.directive('stream', function () {
-    //var startVolume = 25;  //Убрать или нет???
-    return {
-        restrict: 'E',
-        scope: {
-            config: '=config'
-        },
-        template: '<object type="application/x-shockwave-flash" height="670" width="1140" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel={{config.stream.channel.name}}" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel={{config.stream.channel.name}}&auto_play=true&start_volume={{startvolume}}" /></object>'
-    }
-});
+}
