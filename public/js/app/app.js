8b0a1adbb1d0afb4c4d269f2972bd660da3ffcb8
komrakov <alexkomrakov@gmail.com>
2016-09-11 20:38:56 +0300

    + refactoring frontend

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -48530,85 +48530,6 @@ angular.module('streamsApp')
 'use strict';
 
 /**
- * @ngdoc service
- * @name streamsApp.gameSelector
- * @description
- * # gameSelector
- * Service in the streamsApp.
- */
-angular.module('streamsApp')
-    .service('gameSelector', function ($rootScope) {
-        var GameSelector = this;
-        var game = {id: null};
-        GameSelector.getGame = function () {
-            return game;
-        };
-        GameSelector.selectGame = function (game_id) {
-            game.id = game_id;
-            $rootScope.$broadcast("changeGame");
-        }
-    });
-
-'use strict';
-
-/**
- * @ngdoc service
- * @name streamsApp.twitch
- * @description
- * # twitch
- * Service in the streamsApp.
- */
-angular.module('streamsApp')
-    .service('twitch', function ($rootScope, $resource, gameSelector) {
-        var Twitch = this;
-        var data = {streams: [], games: []};
-        var game = gameSelector.getGame();
-        Twitch.Api = $resource('https://api.twitch.tv/kraken/streams', {}, {
-            get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK'}},
-            games: {url: "https://api.twitch.tv/kraken/games/top", method: 'JSONP', params: {callback: 'JSON_CALLBACK'}}
-        });
-        $rootScope.$on('changeGame', function () {
-            data.streams = [];
-            Twitch.updateStreams();
-        });
-        Twitch.updateStreams = function () {
-            $rootScope.$broadcast('loadingStreams', true);
-            Twitch.Api.get({game: game.id, limit: 30}).$promise.then(function (result) {
-                if ('streams' in result && (typeof result.streams[0] != 'undefined')) {
-                    angular.forEach(result.streams, function (value, key) {
-                        if (data.streams[key] && data.streams[key]._id == value._id) {
-                            angular.extend(data.streams[key], value);
-                        } else {
-                            data.streams[key] = value;
-                        }
-                    });
-                }
-                $rootScope.$broadcast('loadingStreams', false);
-            });
-        };
-        Twitch.getStreams = function () {
-            return data;
-        };
-        Twitch.getGames = function () {
-            Twitch.Api.games({limit: 10}).$promise.then(function (response) {
-                if ('top' in response) {
-                    response.top = response.top.splice(0, 10);
-                    angular.forEach(response.top, function (value, key) {
-                        if (data.games[key] && data.games[key].game.name == value.game.name) {
-                            angular.extend(data.games[key], value);
-                        } else {
-                            data.games[key] = value;
-                        }
-                    });
-                }
-            });
-            return data;
-        };
-    });
-
-'use strict';
-
-/**
  * @ngdoc filter
  * @name streamsApp.filter:empty
  * @function
@@ -48693,5 +48614,84 @@ angular.module('streamsApp')
     .filter('urlEncode', function () {
         return function (url) {
             return encodeURIComponent(encodeURIComponent(url));
+        };
+    });
+
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name streamsApp.gameSelector
+ * @description
+ * # gameSelector
+ * Service in the streamsApp.
+ */
+angular.module('streamsApp')
+    .service('gameSelector', function ($rootScope) {
+        var GameSelector = this;
+        var game = {id: null};
+        GameSelector.getGame = function () {
+            return game;
+        };
+        GameSelector.selectGame = function (game_id) {
+            game.id = game_id;
+            $rootScope.$broadcast("changeGame");
+        }
+    });
+
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name streamsApp.twitch
+ * @description
+ * # twitch
+ * Service in the streamsApp.
+ */
+angular.module('streamsApp')
+    .service('twitch', function ($rootScope, $resource, gameSelector) {
+        var Twitch = this;
+        var data = {streams: [], games: []};
+        var game = gameSelector.getGame();
+        Twitch.Api = $resource('https://api.twitch.tv/kraken/streams', {}, {
+            get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK'}},
+            games: {url: "https://api.twitch.tv/kraken/games/top", method: 'JSONP', params: {callback: 'JSON_CALLBACK'}}
+        });
+        $rootScope.$on('changeGame', function () {
+            data.streams = [];
+            Twitch.updateStreams();
+        });
+        Twitch.updateStreams = function () {
+            $rootScope.$broadcast('loadingStreams', true);
+            Twitch.Api.get({game: game.id, limit: 30}).$promise.then(function (result) {
+                if ('streams' in result && (typeof result.streams[0] != 'undefined')) {
+                    angular.forEach(result.streams, function (value, key) {
+                        if (data.streams[key] && data.streams[key]._id == value._id) {
+                            angular.extend(data.streams[key], value);
+                        } else {
+                            data.streams[key] = value;
+                        }
+                    });
+                }
+                $rootScope.$broadcast('loadingStreams', false);
+            });
+        };
+        Twitch.getStreams = function () {
+            return data;
+        };
+        Twitch.getGames = function () {
+            Twitch.Api.games({limit: 10}).$promise.then(function (response) {
+                if ('top' in response) {
+                    response.top = response.top.splice(0, 10);
+                    angular.forEach(response.top, function (value, key) {
+                        if (data.games[key] && data.games[key].game.name == value.game.name) {
+                            angular.extend(data.games[key], value);
+                        } else {
+                            data.games[key] = value;
+                        }
+                    });
+                }
+            });
+            return data;
         };
     });
